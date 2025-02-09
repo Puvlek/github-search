958c3714b5fba1e52c48c8bd001603bb2cac87dd
pchikunov <pchikunov@madela.dev>
2025-02-09 06:04:10 +0300

    created a header

--- src/features/header/header.tsx
+++ src/features/header/header.tsx
@@ -1,16 +1,38 @@
 import React from 'react';
+import cls from './header.module.css'
+import searchIcon from '../../assets/icons/header/search.svg'
+import heartIcon from '../../assets/icons/header/heart.svg'
+import profileIcon from '../../assets/icons/header/profile.svg'
+import {classNames} from "shared/lib/helpers/classNames/classNames";
 import {navigate} from "shared/navigate/navigate";
 import {AppRoutes} from "app/router/model/constants/appRoutes";
 
 const Header = () => {
+
     return (
-        <div>
-            <nav>
-                <button onClick={() => navigate(AppRoutes.repositories.path)}>1</button>
-                <button onClick={() => navigate(AppRoutes.profile.path)}>2</button>
-                <button onClick={() => navigate(AppRoutes.favorites.path)}>3</button>
+            <nav className={cls.headerWrapper}>
+                <button
+                    onClick={() => navigate(AppRoutes.repositories.path)}
+                    className={classNames(cls.logo, {}, [cls.headerButton])}
+                >
+                    <img src={searchIcon} />
+                    <p>GitHubSearch</p>
+                </button>
+                <div className={cls.buttonBlock}>
+                    <button className={cls.headerButton}>
+                        <img
+                            onClick={() => navigate(AppRoutes.favorites.path)}
+                            src={heartIcon}
+                            className={cls.heartIcon}
+                        />
+                    </button>
+                    <button className={cls.headerButton}>
+                        <img
+                            src={profileIcon}
+                            className={classNames(cls.profileIcon, {}, [cls.profilePadding])}/>
+                    </button>
+                </div>
             </nav>
-        </div>
     );
 };
 
